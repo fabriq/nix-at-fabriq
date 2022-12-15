6c000f0c79b16563dd649c08c0de00e6883faf3f
Yacine Hmito <6893840+yacinehmito@users.noreply.github.com>
2022-12-15 09:29:22 +0100

    Come back to default config location

--- nix_settings.nix
+++ nix_settings.nix
@@ -2,8 +2,8 @@
 
 {
   # Use a custom configuration.nix location.
-  # $ darwin-rebuild switch -I darwin-config=$HOME/nix-at-fabriq/configuration.nix
-  environment.darwinConfig = "$HOME/nix-at-fabriq/configuration.nix";
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.nixpkgs/darwin-configuration.nix
+  environment.darwinConfig = "$HOME/.nixpkgs/darwin-configuration.nix";
 
   # Used for backwards compatibility, please read the changelog before changing.
   # $ darwin-rebuild changelog
