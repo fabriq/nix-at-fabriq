d2295861fc6aef17f3856c08971e7e4a0522d41e
Yacine Hmito <6893840+yacinehmito@users.noreply.github.com>
2023-01-06 15:21:15 +0100

    wip

--- hosts/default.nix
+++ hosts/default.nix
@@ -1,6 +1,6 @@
 # This Nix expression must be a attribute set where the keys are device
 # hostnames and the keys are modules with a device-specific configuration.
 {
-  fabriq-mohamed-lt = ./mohamed;
-  fabriq-yacine-lt = ./yacine;
+  fabriq-mohamed-mbp = ./mohamed;
+  fabriq-yacine-mbp-byob = ./yacine;
 }
